f2e4850b22cd655556d375e62a400e0e1c902914
elamonta <ricklamontagne@gmail.com>
2018-05-04 19:45:42 -0400

    styling the todo app

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,47 +1,96 @@
 import React, { Component } from 'react';
 import {
-    View,
-    Text,
-    TextInput,
-    TouchableOpacity
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 export class Todo extends Component {
-    constructor() {
-        super();
-        this.state = {
-            todos: [],
-            newTodo: 'enter todo'
-        }
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      newTodo: 'enter todo'
     }
+  }
 
-    handleChange(text) {
-        this.setState({
-            newTodo: text
-        })
-    }
+  handleChange(text) {
+    this.setState({
+      newTodo: text
+    })
+  }
 
-    handlePress() {
-        const todos = [...this.state.todos, this.state.newTodo];
-        this.setState({todos, newTodo: '' });
-    }
+  handlePress() {
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({ todos, newTodo: '' });
+  }
 
-    render() {
-        return (
-            <View>
-                <TextInput style={{padding: 30,  width: 160}}
-                    value={this.state.newTodo}
-                    onChangeText={this.handleChange.bind(this)}
-                />
-                <TouchableOpacity style={{padding: 30}}
-                    onPress={this.handlePress.bind(this)}>
-                    <Text>Create</Text>
-                </TouchableOpacity>
-                <View style={{padding: 30  }}>
-                    {this.state.todos.map((todo, i) =>
-                        <Text key={i}>{todo}</Text>)}
-                </View>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            value={this.state.newTodo}
+            onChangeText={this.handleChange.bind(this)}
+          />
+          <TouchableOpacity style={styles.button}
+            onPress={this.handlePress.bind(this)}>
+            <Text style={styles.buttonText}>Create</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.todos}>
+          {this.state.todos.map((todo, i) => (
+            <View style={styles.todo} key={i}>
+              <Text style={styles.todoText}>{todo}</Text>
             </View>
-        )
-    }
+          ))}
+        </View>
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  form: {
+    flexDirection: 'row'
+  },
+  input: {
+    flex: 0.7,
+    padding: 0,
+    fontSize: 24,
+    borderBottomWidth: 1,
+    marginRight: 30
+  },
+  button: {
+    marginTop: 20,
+    flex: 0.3,
+    borderWidth: 1,
+    height: 50,
+    borderColor: 'blue',
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  todos: {
+    marginTop: 60
+  },
+  todo: {
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey'
+  },
+  todoText: {
+    fontSize: 24
+  }
+});
