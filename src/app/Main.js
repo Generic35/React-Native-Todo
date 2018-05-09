1c35c408d4d854858fab27346d11e1891810e38b
elamonta <ricklamontagne@gmail.com>
2018-05-09 12:08:35 -0400

    add native gradient plugin

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import TabView  from 'react-native-scrollable-tab-view';
+import LinearGradient from 'react-native-linear-gradient';
 
 export const Main = () => (
-  <View>
+  <LinearGradient colors={['#52EDC7', '#5AC8FB']} style={{ flex: 1 }}>
     <TabView 
       tabBarTextStyle={{fontSize: 30}}>
       <Text tabLabel="hello"></Text>
       <Text tabLabel="yo"></Text>
     </TabView>
-  </View>
+  </LinearGradient>
 )
--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -1,33 +1,40 @@
 import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import Button from 'apsl-react-native-button';
 import {
   Text,
   View,
-  StyleSheet,
-  TouchableOpacity
+  StyleSheet
 } from 'react-native';
 
 export const Welcome = (props) => (
-  <View style={{ flex: 1 }}>
-    <View style={styles.titleContianer}>
-      <Text style={styles.title}>
-        Sweatbook
-      </Text>
-    </View>
-    <View style={styles.lastWorkoutContainer}>
-      <Text style={styles.lastWorkoutTitle}>
-        Log your fitness
-      </Text>
-    </View>
-    <View style={{ padding: 30 }}>
-      <TouchableOpacity
-        onPress={props.startWorkout}
-        style={styles.button}
-      >
-        <Text>Start Workout</Text>
-      </TouchableOpacity>
-    </View>
+  <View style={{flex: 1}}>
+    <LinearGradient
+      colors={['#4A148C', '#880E4F']}
+      style={styles.container}
+    >
+      <View style={styles.titleContianer}>
+        <Text style={styles.title}>
+          Sweatbook
+        </Text>
+      </View>
+      <View style={styles.lastWorkoutContainer}>
+        <Text style={styles.lastWorkoutTitle}>
+          Your last workout
+        </Text>
+      </View>
+      <View style={{padding: 30 }}>
+        <Button
+          onPress={props.startWorkout}
+          style={styles.button}
+          textStyle={styles.buttonText}
+          children={'Start Workout'}
+        />
+      </View>
+    </LinearGradient>
   </View>
 );
+
 
 const styles = StyleSheet.create({
   container: {
@@ -37,28 +44,29 @@ const styles = StyleSheet.create({
   titleContianer: {
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 0.2
+    flex: 0.5
   },
   title: {
     fontSize: 48,
     fontWeight: '500',
-    color: 'blue',
+    color: '#F3F3F3',
     textAlign: 'center'
   },
   lastWorkoutContainer: {
-    flex: 0.3
+    flex: 0.5
   },
   lastWorkoutTitle: {
     fontSize: 30,
     fontWeight: '100',
-    color: 'blue'
+    color: '#F3F3F3'
   },
   button: {
     borderColor: '#EADCDC',
     padding: 10,
-    borderRadius: 10
+    borderRadius: 10,
+    width: 200
   },
   buttonText: {
-    color: '#EADCDC'
+    color: '#EADCDC',
   }
 });
